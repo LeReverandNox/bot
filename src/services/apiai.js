2f2d24149eef88072e80e36bce4a89d83f0004e1
laidet_r <rodolphe.laidet@epitech.eu>
2017-09-13 19:46:02 +0200

    Implement an ApiAI service, that can verify our intent against an agent

---
+++ src/services/apiai.js
@@ -0,0 +1,47 @@
+/*jslint node: true this:true es6:true */
+/*global this*/
+
+"use strict";
+
+const ai = require("apiai");
+
+module.exports = (server) => {
+    const config = server.app.config;
+    const services = server.app.services;
+
+    const apiai = {
+        init: function () {
+            this.clientAccessToken = config.apiAi.clientAccessToken;
+            this.agent = ai(this.clientAccessToken);
+
+            return this;
+        },
+        isAdoptingAnimal: async function (text) {
+            return new Promise((resolve, reject) => {
+                const request = this.agent.textRequest(text, {
+                    sessionId: '<unique session id>'
+                });
+
+                request.on('response', (response) => {
+                    const result = response.result;
+                    const metadata = result.metadata;
+                    const intentName = metadata.intentName
+
+                    if (intentName === "Adopter un animal") {
+                        return resolve();
+                    }
+                    return reject();
+                });
+
+                request.on('error', (err) => {
+                    console.error(err);
+                    return reject(err);
+                });
+
+                request.end();
+            });
+        }
+    };
+
+    return apiai.init();
+};
