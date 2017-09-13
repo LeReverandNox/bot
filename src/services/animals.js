422cb0ef7b6d31a2b6a82754876dee05400b72ea
laidet_r <rodolphe.laidet@epitech.eu>
2017-09-13 23:23:59 +0200

    Implements methods to get dogs and cats in animalsService

--- src/services/animals.js
+++ src/services/animals.js
@@ -3,7 +3,9 @@
 
 "use strict";
 
-const ai = require("apiai");
+const rp = require("request-promise");
+const catNames = require("cat-names");
+const dogNames = require("dog-names");
 
 module.exports = (server) => {
     const config = server.app.config;
@@ -11,35 +13,51 @@ module.exports = (server) => {
 
     const animalsService = {
         init: function () {
+            this.dogApiUrl = config.dog.apiUrl;
+            this.catApiUrl = config.cat.apiUrl;
+
             return this;
         },
         getDogs: async function (nb) {
-            return [
-                {
-                    name: "Ouaf",
+            const dogs = [];
+
+            for (let i = 0; i < nb ; i += 1) {
+                let options = {
+                    json: true,
+                    uri: `${this.dogApiUrl}/breeds/image/random`
+                };
+
+                let dogImageUrl = await rp.get(options).then(res => res.message);
+                let dog = {
+                    name: dogNames.allRandom(),
                     type: "Chien",
-                    imageUrl: "https://static.pexels.com/photos/356378/pexels-photo-356378.jpeg"
-                },
-                {
-                    name: "Paf",
-                    type: "Chien",
-                    imageUrl: "https://usercontent2.hubstatic.com/8709045_f496.jpg"
-                }
-            ];
+                    imageUrl: dogImageUrl
+                };
+
+                dogs.push(dog);
+            }
+
+            return dogs;
         },
         getCats: async function (nb) {
-            return [
-                {
-                    name: "Ronron",
+            const cats = [];
+
+            for (let i = 0; i < nb; i += 1) {
+                let options = {
+                    uri: `${this.catApiUrl}`
+                };
+
+                let catImageUrl = await rp.get(options).then(res => JSON.parse(res)).then(res => res.file);
+                let cat = {
+                    name: catNames.random(),
                     type: "Chat",
-                    imageUrl: "http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg"
-                },
-                {
-                    name: "Minou",
-                    type: "Chat",
-                    imageUrl: "https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=38632134"
-                }
-            ];
+                    imageUrl: catImageUrl
+                };
+
+                cats.push(cat);
+            }
+
+            return cats;
         }
     };
 
